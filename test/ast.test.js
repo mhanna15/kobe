ceb577269e206fe884660450984710c687cc728f
Alizandro Lopez <alizanpez@gmail.com>
2022-04-06 14:33:58 -0700

    making sure ast.test is up to date with master

--- test/ast.test.js
+++ test/ast.test.js
@@ -6,14 +6,12 @@ const source = `num x=5
                 num job addFive(num x) {output x add 5}`;
 
 const expected = `   1 | Program statements=[#2,#3]
-   2 | VariableDeclaration type='num' variable=(Id,"x") initializer=(Num, "5")
-   3 | FunctionDeclaration fun=#4 body=
-   4 | Function id=(Id,"addFive") param=[#5] returnType='num'
-   5 | Paramerter type='num' name=(Id,"x")
-   6 | Assignment target=(Id,"x") source=(Num,"3")
-   7 | PrintStatement argument=#9
-   8 | Conditional test=(Num,"0") consequent=#10 alternate=(Num,"2")
-   9 | Call callee=(Id,"f") args=[(Id,"x")]`;
+   2 | VariableDeclaration type='num' variable=(Id,"x") initializer=(Num,"5")
+   3 | FunctionDeclaration fun=#4 chunk=[#6]
+   4 | Function returnType='num' id=(Id,"addFive") param=[#5]
+   5 | Parameter type='num' name=(Id,"x")
+   6 | returnStatement expression=#7
+   7 | BinaryExpression op='add' left=(Id,"x") right=(Num,"5")`;
 
 describe("The AST generator", () => {
   it("produces the expected AST for all node types", () => {
